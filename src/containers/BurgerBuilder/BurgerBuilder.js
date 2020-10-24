e0399244b0d12d0d422ca0a2485645a387051f61
Jackson Cai <jcai8649@gmail.com>
2020-10-24 00:46:24 -0700

    Create order function

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,19 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchaseable: false
+    }
+
+    updatePurchaseState (ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({purchaseable: sum > 0})
     }
 
     addIngredientHandler = (type) => {
@@ -31,7 +43,8 @@ class BurgerBuilder extends Component {
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
-        this.setState( {ingredients : updatedIngredients, totalPrice : newPrice}) 
+        this.setState( {ingredients : updatedIngredients, totalPrice : newPrice});
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -50,6 +63,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDedection;
         this.setState( {ingredients : updatedIngredients, totalPrice : newPrice}) 
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render (){
@@ -67,7 +81,9 @@ class BurgerBuilder extends Component {
                 <BuildControls 
                     ingredientAdded = {this.addIngredientHandler} 
                     ingredientRemoved = {this.removeIngredientHandler}
-                    disabled={disabledInfo}/>
+                    disabled={disabledInfo}
+                    purchaseable={this.state.purchaseable}
+                    price={this.state.totalPrice}/>
             </Fragment>
         );
     }
