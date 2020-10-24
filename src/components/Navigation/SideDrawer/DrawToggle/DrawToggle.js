3b3b163ed000c2562b8ef9489d779412738359e7
Jackson Cai <jcai8649@gmail.com>
2020-10-24 08:19:05 -0700

    Restructure folders

--- src/components/Navigation/SideDrawer/DrawToggle/DrawToggle.js
+++ src/components/Navigation/SideDrawer/DrawToggle/DrawToggle.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from './DrawToggle.module.css';
 
 const drawToggle = (props) => (
-    <div onClick={props.clicked}>Menu</div>
+    <div className={classes.DrawToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default drawToggle;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Modal.module.css'
 import Backdrop from '../../UI/Backdrop/Backdrop'
 
-const modal = (props) => (
-    <Fragment>
-        <Backdrop show={props.show}  clicked={props.modalClosed}/>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
-        </div>
-    </Fragment>
-);
+class modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return  nextProps.show !== this.props.show;
+            
+    }
+
+
+    render() {
+     return (
+        <Fragment>
+            <Backdrop show={this.props.show}  clicked={this.props.modalClosed}/>
+            <div 
+                className={classes.Modal}
+                style={{
+                    transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: this.props.show ? '1' : '0'
+                }}>
+                {this.props.children}
+            </div>
+        </Fragment>
+     )
+    }
+};
 
 export default modal;
