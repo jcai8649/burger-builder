8c8e9124a3ec8d796af1fc135901d5956cd14683
Jackson Cai <jcai8649@gmail.com>
2020-10-24 06:43:30 -0700

    Change  dynamic sidedraw

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,32 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
-const layout = (props) => (
-    <Fragment>
-        <div>
-            <Toolbar/>
-            <SideDrawer/>
-             Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Fragment>
-);
+class layout extends Component {
+    state = {
+        showSideDrawer: true
+     }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render(){
+        return (
+            <Fragment>
+                <Toolbar/>
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        )
+    }
+}
+
 
 export default layout;
