7e6aee34348df14d4d069a8d20d294df1509f09a
Jackson Cai <jcai8649@gmail.com>
2020-10-26 19:00:29 -0700

    Implement spinner into modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,7 +5,7 @@ import Backdrop from '../../UI/Backdrop/Backdrop'
 class modal extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
-        return  nextProps.show !== this.props.show;
+        return  nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
     render() {
