899815f7fa0fd4ae0a7e5ad3e482772e7bc4b7a0
Jackson Cai <jcai8649@gmail.com>
2020-10-28 16:34:29 -0700

    Fix order summary button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,37 +90,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Jackson Kai',
-        //         address: {
-        //             street: 'Teststreet 23',
-        //             zipCode: '41351',
-        //             country: 'US'
-        //         },
-        //         email: 'test@test.com'
-        //     }, 
-        //     deliveryMethod: 'ASAP'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading: false,
-        //             purchasing: false,
-        //         });
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false,
-        //             purchasing: false,
-        //         });
-        //     });
 
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&')
         this.props.history.push({
             pathname: '/checkout',
