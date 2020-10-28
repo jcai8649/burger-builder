93046b112f20211d73b7394ac638663e6491dbe0
Jackson Cai <jcai8649@gmail.com>
2020-10-28 15:53:47 -0700

    Add style to order sumarry

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
 class Checkout extends Component {
@@ -9,6 +11,16 @@ class Checkout extends Component {
             cheese: 1,
             bacon: 1
         }
+    }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1];
+        }
+
+        this.setState({ingredients: ingredients});
     }
 
     checkoutCancelledHandler = () => {
@@ -26,6 +38,7 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}/>
+                <Route path={this.props.match.url + '/contact-data' } component={ContactData}/>
             </div>
         )
     }
