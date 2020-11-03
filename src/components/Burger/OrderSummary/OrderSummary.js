de361efa20662f0f1aa289c71d94877002451c92
Jackson Cai <jcai8649@gmail.com>
2020-11-02 20:47:22 -0800

    Implement code split for routes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,7 +3,6 @@ import Button from '../../UI/Button/Button';
 
 
 class OrderSummary extends Component {
-
     render() {
         const ingredientSummary = Object.keys(this.props.ingredients)
             .map(igKey => {
